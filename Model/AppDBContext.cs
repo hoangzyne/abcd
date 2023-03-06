c211445b3ce1b3e34ad8a735be4467cec95b743e
hoangzyne <vyhoangngoc2000@gmail.com>
2023-03-06 15:56:29 +0700

    first commit

---
+++ Model/AppDBContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuAnMoi.Model
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        public string ConnectionString { get; private set; }
+        public AppDBContext()
+        {
+            var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            ConnectionString = Path.Combine(folder, "repo.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data source= {ConnectionString}");
+        }
+    }
+
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+
+    public class Category : BaseEntity
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+    
+    public class Customer : BaseEntity
+    {
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [StringLength(250)]
+        public string Address { get; set; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DuAnMoi.Model;
+using DuAnMoi.Repository;
+
+
+
+
+var db = new AppDBContext();
+Repository<Category> repositoryCategory = new Repository<Category>(db);
+
+//await repositoryCategory.Create(new Category { Name = "Moba"});
+//await repositoryCategory.Create(new Category { Name = "RPG" });
+//await repositoryCategory.Create(new Category { Name = "Music" });
+//await repositoryCategory.Create(new Category { Name = "FPS" });
+
+Repository<Customer> repositoryCustomer = new Repository<Customer>(db);
+await repositoryCustomer.Create(new Customer
+{
+    FirstName = "Dung",
+    LastName = "Nguyen"
+});
+await repositoryCustomer.Create(new Customer
+{
+    FirstName = "Tèo",
+    LastName = "Trần Văn"
+});
+
+Console.WriteLine("Created: 2 Customer");
